28d259989fa1fa86c8e994ddd9b96c4fbf59cf28
João Vitor Pereira <115598741+JaovitoP@users.noreply.github.com>
2022-11-28 16:21:23 -0300

    Update delete.php

--- Site/PHP/JoaoVitor (em andamento)/delete.php
+++ Site/PHP/JoaoVitor (em andamento)/delete.php
@@ -1,21 +1,22 @@
 <?php
 
-    if(!empty($_GET['id']))
+    if(!empty($_GET['user_id']))
     {
         include_once('config.php');
 
-        $id = $_GET['id'];
+        $id = $_GET['user_id'];
 
-        $sqlSelect = "SELECT *  FROM usuarios WHERE id=$id";
+        $sqlSelect = "SELECT *  FROM TB_user_login WHERE user_id=$id";
 
         $result = $conexao->query($sqlSelect);
 
         if($result->num_rows > 0)
         {
-            $sqlDelete = "DELETE FROM usuarios WHERE id=$id";
+            $sqlDelete = "DELETE FROM TB_user_login WHERE user_id=$id";
             $resultDelete = $conexao->query($sqlDelete);
         }
     }
     header('Location: sistema.php');
-   
+       
+      
 ?>
